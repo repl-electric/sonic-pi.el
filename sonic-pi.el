4af1e6c9af6c92cdd4864f1898df95f87c7c05c5
Joseph Wilk <joe@josephwilk.net>
2014-09-25 21:33:36 +0200

    Better name for publishing as an emacs package

--- sonic-pi.el
+++ sonic-pi.el
@@ -1,4 +1,4 @@
-;;; sonic-emacs-pi.el --- A Emacs client for SonicPi
+;;; sonic-pi.el --- A Emacs client for SonicPi
 ;; Version: 0.1.0
 ;; Package-Requires: ((cl-lib "0.5") (osc "0.1") (emacs "24"))
 ;; Keywords: SonicPi, Ruby
@@ -6,18 +6,18 @@
 (require 'sonic-mode)
 (require 'sonic-osc)
 
-(defgroup sonic-emacs-pi nil
+(defgroup sonic-pi nil
   "A client for interacting with the SonicPi Server."
-  :prefix "sonic-"
+  :prefix "sonic-pi-"
   :group 'applications
-  :link '(url-link :tag "Github" "https://github.com/repl-electric/sonic-emacs-pi")
+  :link '(url-link :tag "Github" "https://github.com/repl-electric/sonic-pi.el")
   :link '(emacs-commentary-link :tag "Commentary" "Sonic Emacs Pi"))
 
 (defcustom sonic-pi-path "/Users/josephwilk/Workspace/josephwilk/ruby/sonic-pi" "Path to install of sonicpi")
 (defvar    sonic-pi-server-bin "app/server/bin/sonic-pi-server.rb")
 
 ;;;###autoload
-(defun sonic-jack-in (&optional prompt-project)
+(defun sonic-pi-jack-in (&optional prompt-project)
   "Boot and connect to the SonicPi Server"
   (interactive)
   (let* ((cmd (format "rvm use 1.9.3-p545; %s/%s" sonic-pi-path sonic-pi-server-bin)))
@@ -25,10 +25,10 @@
      "sonic-server"
      "sonic-sonic-boom"
      cmd))
-  (sonic-connect))
+  (sonic-pi-connect))
 
 ;;;###autoload
-(defun sonic-connect (&optional prompt-project)
+(defun sonic-pi-connect (&optional prompt-project)
   "Connect to SonicPi Server"
   (interactive)
   (sonic-osc-connect))
@@ -36,9 +36,9 @@
 ;;;###autoload
 (eval-after-load 'ruby-mode
   '(progn
-     (define-key ruby-mode-map (kbd "C-c M-j") 'sonic-jack-in)
-     (define-key ruby-mode-map (kbd "C-c M-c") 'sonic-connect)))
+     (define-key ruby-mode-map (kbd "C-c M-j") 'sonic-pi-jack-in)
+     (define-key ruby-mode-map (kbd "C-c M-c") 'sonic-pi-connect)))
 
-(provide 'sonic-emacs-pi)
+(provide 'sonic-pi)
 
-;;; sonic-emacs-pi.el ends here
+;;; sonic-pi.el ends here
