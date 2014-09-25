845a04e4403f7200a23503aa1fd1e977c0efdafe
Joseph Wilk <joe@josephwilk.net>
2014-09-25 21:36:03 +0200

    New name

--- sonic-emacs-pi-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "sonic-emacs-pi" "0.1.0" "A Emacs client for SonicPi"
-  '((emacs "24")
-    (osc "0.1")
-    (cl-lib "0.5")))
---
+++ sonic-pi-pkg.el
@@ -0,0 +1,4 @@
+(define-package "sonic-pi" "0.1.0" "A Emacs client for SonicPi"
+  '((emacs "24")
+    (osc "0.1")
+    (cl-lib "0.5")))
