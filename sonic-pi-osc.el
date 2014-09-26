5bdaa2e72c47e34908c7c8e766f0d59c08aa7748
Joseph Wilk <joe@josephwilk.net>
2014-09-26 17:26:30 +0200

    nicer message when sonic-pi is not running

--- sonic-pi-osc.el
+++ sonic-pi-osc.el
@@ -15,7 +15,9 @@
                                    (lambda (path &rest args) (sonic-pi-log-message path args))))))
 
 (defun sonic-pi-osc-send-text (start end)
-  (osc-send-message sonic-pi-osc-client "/run-code" (buffer-substring-no-properties start end)))
+  (if sonic-pi-osc-client
+      (osc-send-message sonic-pi-osc-client "/run-code" (buffer-substring-no-properties start end))
+    (message "Sonic-pi not running... `sonic-pi-jack-in` or `sonic-pi-connect`")))
 
 (defun sonic-pi-send-region ()
   "send a region to sonic via osc"
